25ac3d64222e9687932f2def115ac9388489c7c2
alla <a2la1101@gmail.com>
2017-11-16 18:54:15 +0200

    Created main class + src File for program1

--- App.js
+++ App.js
@@ -1,21 +1,2 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Trying out git with React Native as a begining, let's see how it rolls!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import cmain from './src/cmain';
+export default cmain;
